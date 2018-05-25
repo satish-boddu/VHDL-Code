0a7021330b11e9a64fe5f02b2a13fb3808e5ef02
Satish Boddu <38870071+satish-boddu@users.noreply.github.com>
2018-05-25 15:39:21 -0400

    Editing the Comments on Lab1 VHDL Code

--- Lab1/VHDL_gates.vhd
+++ Lab1/VHDL_gates.vhd
@@ -3,7 +3,7 @@ USE ieee.std_logic_1164.all;
 
 LIBRARY work;
 
-ENTITY VHDL_gates IS
+ENTITY VHDL_gates IS                       #Defining all input and output variables
 	PORT
 	(
 		AND_IN1, AND_IN2, NAND_IN1, NAND_IN2, OR_IN1, OR_IN2, XOR_IN1, XOR_IN2  :  IN  BIT;
@@ -13,9 +13,9 @@ END VHDL_gates;
 
 ARCHITECTURE simple_gates OF VHDL_gates IS
 
-BEGIN
+BEGIN                                      #Writing the logic for each of the output variables
 
-AND_OUT <= AND_IN1 AND AND_IN2;
+AND_OUT <= AND_IN1 AND AND_IN2;            #Taking the Inputs and Performing the Logic
 NAND_OUT <= NAND_IN1 NAND NAND_IN2;
 OR_OUT <= OR_IN1 OR OR_IN2;
 XOR_OUT <= XOR_IN1 XOR XOR_IN2;
